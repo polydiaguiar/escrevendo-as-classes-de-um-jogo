79c409a6f920363d928e9aa1f2a6c87b0d38ac6f
unknown <polyana.karynne@hotmail.com>
2024-01-21 17:44:51 -0300

    test: adiciona classe e método

---
+++ index.js
@@ -0,0 +1,25 @@
+class heroCharacter (){
+    constructor (heroName, age, type){
+        this.heroName = heroName
+        this.age = age
+        this.type = type
+    }
+
+    atacar(){
+        se mago -> no ataque exibir (usou magia)
+        se guerreiro -> no ataque exibir (usou espada)
+        se monge -> no ataque exibir (usou artes marciais)
+        se ninja -> no ataque exibir (usou shuriken)
+       
+        
+
+
+     }
+
+     escrever(){
+        console.log(`O ${type} atacou usando ${ataque}`)
+
+     }
+
+
+}
